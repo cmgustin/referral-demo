a95e3192dc2eea7bfbbc0c7e2d28ae268bb002bf
Chris Gustin <gustin.chris@gmail.com>
2020-01-22 17:12:18 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $('#form').on('submit', function(e) {
+    e.preventDefault();
+    var nameString = $('input[name="lastName"]').val();
+    var nameSubString = nameString.substring(0, 2).toUpperCase();
+    var timestamp = new Date().getTime();
+    var randNumber = Math.round(timestamp / 1000000);
+    var referralId = nameSubString + '-' + randNumber;
+    $('#referralId').val(referralId);
+
+    var formData = $(this).serialize();
+    $.post('https://hooks.zapier.com/hooks/catch/1240383/ohfsqh6/', formData)
+      .done(function(response) {
+        $('#form').hide();
+        $('#success').fadeIn(250);
+      })
+      .fail(function(error) {
+        console.log(error);
+      });
+  });
+});
